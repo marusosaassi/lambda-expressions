8b82a087932584f70d294401a8faf96d54363331
Maru Sosa <ms@wayakna.com>
2022-10-10 14:30:03 -0500

    Consumer

--- src/java8/lambda/ConsumerExample.java
+++ src/java8/lambda/ConsumerExample.java
@@ -1,4 +1,27 @@
 package java8.lambda;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
 public class ConsumerExample {
+    public static void main(String[] args) {
+
+        // Consumer<String> consumer = greeting -> System.out.println(greeting);
+        /* Consumer is a lambda expression that allows us to pass one parameter alone as argument */
+        /* Inside this arrow we can do something with the argument we are passing */
+        /* This method is void, it doesn't return anything */
+
+        Consumer<Date> consumer = date -> {
+            SimpleDateFormat d = new SimpleDateFormat("yyyy-MM-dd");
+            System.out.println(d.format(date));
+        };
+        consumer.accept(new Date());
+
+        BiConsumer<String, Integer> biConsumer = (name, age) -> {
+            System.out.println(name + " is " + age + " years old.");
+        };
+        biConsumer.accept("Rafa", 26);
+    }
 }
