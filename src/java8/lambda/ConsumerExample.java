eb6dd6fbe95f2487f5ab38a2eb394c53fbb9956f
marusosasssi <soassi@outlook.com>
2022-10-10 23:22:43 -0500

    method references in consumer lambda expressions

--- src/java8/lambda/ConsumerExample.java
+++ src/java8/lambda/ConsumerExample.java
@@ -1,9 +1,14 @@
 package java8.lambda;
 
+import java8.lambda.models.User;
+
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class ConsumerExample {
     public static void main(String[] args) {
@@ -23,5 +28,35 @@ public class ConsumerExample {
             System.out.println(name + " is " + age + " years old.");
         };
         biConsumer.accept("Rafa", 26);
-    }
+
+        //Consumer<String> consumer2 = message -> System.out.println(message); // simplified;
+        Consumer<String> consumer2 = System.out::println;
+        consumer2.accept("Hello world lambda");
+
+        List<String> names = Arrays.asList("maru", "rafael", "damian", "vianey", "addy");
+        names.forEach(consumer2);
+
+        User user = new User();
+
+        /*BiConsumer<User, String> assignName = (person, name) -> {
+            person.setName(name);
+        };*/
+        //simplifying:
+        BiConsumer<User, String> assignName = User::setName;
+
+        assignName.accept(user, "Maru");
+        System.out.println("Name of the user: " + user.getName());
+
+        /*Supplier<String> provider = () -> {
+            return "hello world";
+        };*/
+
+        Supplier<String> provider = () -> "hello world";
+        System.out.println(provider.get());
+
+        Supplier<User> createUser = User::new;
+        User user2 = createUser.get();
+        System.out.println(user2.getName());
+
+     }
 }
---
+++ src/java8/lambda/models/User.java
@@ -0,0 +1,14 @@
+package java8.lambda.models;
+
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
